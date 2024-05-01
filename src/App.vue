4428cf051e57917aa5adb18faa103616e95ceb6a
e.syroezhkin <e.syroezhkin@g.nsu.ru>
2024-05-02 00:01:57 +0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TabButton from "@/components/TabButton.vue";
+</script>
+
+<template>
+  <header>
+    <div class="header">
+      <TabButton :name="'test'" :link="'/#link'" />
+    </div>
+  </header>
+
+  <main>
+
+
+
+  </main>
+</template>
+
+<style>
+  @import "./style/header.css";
+
+</style>
---
+++ src/components/TabButton.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: ['name', 'link']
+}
+</script>
+
+<template>
+<div class="header_button">
+  <a :href=link>{{name}}</a>
+</div>
+</template>
+
+<style>
+.header_button {
+  margin: 5px;
+  padding: 10px 25px 10px 25px;
+  background: rgba(255, 255, 255, 0.26);
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+a {
+  text-decoration: none;
+}
+
+.header_button:hover {
+  background: #669bbc;
+}
+</style>
