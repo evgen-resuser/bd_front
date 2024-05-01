83641391aaa0ee211e4504b24ede78244b2e5d69
e.syroezhkin <e.syroezhkin@g.nsu.ru>
2024-05-02 00:54:38 +0700

    Tabs buttons style and icons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
-import TabButton from "@/components/TabButton.vue";
+import TabButton from "@/components/TabButton.vue"
 </script>
 
 <template>
   <header>
     <div class="header">
-      <TabButton :name="'test'" :link="'/#link'" />
+      <TabButton :name="'Спортсмены'" :link="'/#sportsman'" :icon_path="'public/icons/sportsman_icon.svg'"/>
+      <TabButton :name="'Тренеры'" :link="'/#coach'" :icon_path="'public/icons/coach_icon.svg'"/>
+      <TabButton :name="'Места'" :link="'/#place'" :icon_path="'public/icons/place_icon.svg'"/>
+      <TabButton :name="'Соревнования'" :link="'/#competition'" :icon_path="'public/icons/trophy_icon.svg'"/>
     </div>
   </header>
 
--- src/components/TabButton.vue
+++ src/components/TabButton.vue
@@ -1,12 +1,26 @@
 <script>
 export default {
-  props: ['name', 'link']
+  props: ['name', 'link', 'icon_path'],
+  methods: {
+    goToLink() {
+      window.location.href = this.link
+    }
+  },
+  head() {
+    return {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap' }
+      ]
+    }
+  }
 }
 </script>
 
 <template>
-<div class="header_button">
-  <a :href=link>{{name}}</a>
+<div class="header_button" @click="goToLink">
+  <img class="icon" :src="icon_path" alt=""/><span class="name">{{name}}</span>
 </div>
 </template>
 
@@ -17,10 +31,26 @@ export default {
   background: rgba(255, 255, 255, 0.26);
   border-radius: 5px;
   cursor: pointer;
+  width: 120px;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
 }
 
-a {
-  text-decoration: none;
+.name {
+  font-family: "Rubik", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 100;
+  font-style: normal;
+}
+
+.icon {
+  margin: 0 5px 0 0;
+  filter: invert();
 }
 
 .header_button:hover {
